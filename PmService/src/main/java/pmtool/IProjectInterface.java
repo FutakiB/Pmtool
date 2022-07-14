c2abcf115a4f51ae898fa1f11f166576263a4d15
Orban Balazs <orban.balazs.96@gmail.com>
2022-07-14 16:24:17 +0200

    Add 2 interfaces

--- PmService/src/main/java/pmtool/IProjectInterface.java
+++ PmService/src/main/java/pmtool/IProjectInterface.java
@@ -1,4 +1,17 @@
 package pmtool;
 
+import java.awt.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface IProjectInterface {
+    Project createProject(int id, String name, ProjectStatus status, LocalDateTime startTime, String clientName, String backOfficeName, List<Task> milestones);
+    void addTask(Task task);
+    void modifyState(ProjectStatus status);
+    void modifyStartTime(LocalDateTime starttime);
+    List<Task> getMilestone();
+    void deleteProject(int id);
+
+
+
 }
