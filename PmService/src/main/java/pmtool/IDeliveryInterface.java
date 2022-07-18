69ec439708019cf0c540257046beea6326b78ae2
Bitai Bence <bitai.bence@gmail.com>
2022-07-18 16:16:42 +0200

    Removed interfaces

--- PmService/src/main/java/pmtool/IDeliveryInterface.java
+++
@@ -1,7 +0,0 @@
-package pmtool;
-
-public interface IDeliveryInterface {
-    Delivery crateDelivery(int id, String name, DeliveryType type);
-    void addToMilestone(Milestone milestone);
-    void deliver();
-}
