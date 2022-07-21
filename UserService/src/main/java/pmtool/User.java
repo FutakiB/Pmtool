1c1354a616f8b4ab253cc43911b94118e85cc257
Bitai Bence <bitai.bence@gmail.com>
2022-07-21 12:00:02 +0200

    User constructor

--- UserService/src/main/java/pmtool/User.java
+++ UserService/src/main/java/pmtool/User.java
@@ -1,14 +1,21 @@
 package pmtool;
 
-public class User{
+public class User {
     public enum role {
         Dev,
         Manager,
         Analyst
     }
+
     private String name;
     private Integer id;
     private role role;
+
+    public User(String name, Integer id, User.role role) {
+        this.name = name;
+        this.id = id;
+        this.role = role;
+    }
 
     public Integer getId() {
         return id;
