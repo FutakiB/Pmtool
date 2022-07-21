3a4fbff82cf6e19b0c767ab8f008e39915342466
Fbalint <futaki.balint20@gmail.com>
2022-07-21 11:32:59 +0200

    Test added

---
+++ PmService/src/test/java/pmtool/PmServiceTest.java
@@ -0,0 +1,87 @@
+package pmtool;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PmServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void addProject() {
+    }
+
+    @Test
+    void addMilestone() {
+    }
+
+    @Test
+    void addDelivery() {
+    }
+
+    @Test
+    void addTask() {
+    }
+
+    @Test
+    void testAddTask() {
+    }
+
+    @Test
+    void removeProject() {
+    }
+
+    @Test
+    void removeMilestone() {
+    }
+
+    @Test
+    void removeDelivery() {
+    }
+
+    @Test
+    void removeTask() {
+    }
+
+    @Test
+    void getAllProjects() {
+    }
+
+    @Test
+    void getAllMilestones() {
+    }
+
+    @Test
+    void getAllDeliveries() {
+    }
+
+    @Test
+    void getAllTasks() {
+    }
+
+    @Test
+    void getAllSubTasks() {
+    }
+
+    @Test
+    void getAllMilestonesByProject() {
+    }
+
+    @Test
+    void getAllDeliveriesByMilestone() {
+    }
+
+    @Test
+    void getAllTasksByMilestone() {
+    }
+
+    @Test
+    void getAllSubTasksByTask() {
+    }
+
+    @Test
+    void getAllTasksByProject() {
+    }
+}
